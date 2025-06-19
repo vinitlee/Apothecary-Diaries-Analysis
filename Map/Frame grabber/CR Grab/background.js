a93bc5f45faf04d1bb1a9aefcfc77d05d2edf2a4
Vincent Lee <vinitlee@gmail.com>
2025-06-19 14:31:06 -0700

    Giving up on rewriting the video frame grabber

--- Map/Frame grabber/CR Grab/background.js
+++
@@ -1,9 +0,0 @@
-chrome.webNavigation.onCompleted.addListener(
-  (details) => {
-    chrome.scripting.executeScript({
-      target: { tabId: details.tabId, frameIds: [details.frameId] },
-      files: ["content-iframe.js"],
-    });
-  },
-  { url: [{ hostSuffix: "static.crunchyroll.com" }] }
-);
